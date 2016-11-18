04ede7e854c7f2807163b5a1d845a9ea75328a2c
samith <kottackalsamith@gmail.com>
2016-11-18 12:02:22 +0530

    integrated with systemjs and working properly

--- public/app/src/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var angular = require('angular');
-var app = angular.module('helloApp', []);
-function bootstrap(target) {
-    angular.bootstrap(target, [app.name], { strictDi: true });
-}
-exports.bootstrap = bootstrap;
-//# sourceMappingURL=app.js.map
--- public/app/src/maincontroller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var MainController = (function () {
-    function MainController() {
-        this.message = "Hello AngularJS, TypeScript & SystemJS";
-    }
-    return MainController;
-}());
-exports.MainController = MainController;
-//# sourceMappingURL=maincontroller.js.map
