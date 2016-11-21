12c9d026be8ea26ccfa1648bc22a62da4043e276
samith <kottackalsamith@gmail.com>
2016-11-21 15:50:33 +0530

    userservices changed to typescript and working

--- public/app/services/userService.js
+++ public/app/services/userService.js
@@ -3,30 +3,33 @@ System.register([], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var UserService;
-    function userService($http) {
-        var userFactory = {};
-        userFactory.create = function (userData) {
-            return $http.post('/api/signup', userData);
-        };
-        console.log(userFactory);
-        return userFactory;
+    function createUser($http) {
+        return new UserService($http);
     }
     return {
         setters:[],
         execute: function() {
+            // function userService($http) {
+            //         var userFactory = {};
+            //         userFactory.create = function(userData){
+            //             return $http.post('/api/signup', userData);
+            //         };
+            //         console.log(userFactory);
+            //         return userFactory;
+            // }
             UserService = (function () {
                 function UserService($http) {
                     this.$http = $http;
-                    this.userFactory = {};
+                    return;
                 }
                 UserService.prototype.create = function (userdata) {
+                    console.log(userdata);
                     return this.$http.post('/api/signup', userdata);
                 };
-                UserService.$inject = ['$http'];
                 return UserService;
             }());
             exports_1("default",angular.module('userService', [])
-                .factory('User', ['$http', userService]));
+                .factory('User', ['$http', createUser]));
         }
     }
 });
--- public/src/services/userService.ts
+++ public/src/services/userService.ts
@@ -1,36 +1,35 @@
 /// <reference path="../_all.ts" />
 
 interface UserServiceInterface {
-    userFactory: Object;
     create(userdata:Object):Object;
 }
 
-function userService($http) {
+// function userService($http) {
    
-        var userFactory = {};
-        userFactory.create = function(userData){
-            return $http.post('/api/signup', userData);
-        };
-        console.log(userFactory);
-        return userFactory;
+//         var userFactory = {};
+//         userFactory.create = function(userData){
+//             return $http.post('/api/signup', userData);
+//         };
+//         console.log(userFactory);
+//         return userFactory;
     
-}
+// }
 
 class UserService implements UserServiceInterface {
-    static $inject = ['$http'];
-
-    userFactory: Object;
-
-    constructor(private $http:ng.IHttpService){
-        this.userFactory = {};
-    }
     
-    create(userdata:Object):Object{
+    constructor(private $http: ng.IHttpService) {
+        return;
+    }
+    create(userdata: Object): Object {
+        console.log(userdata);
         return this.$http.post('/api/signup', userdata);
     }
-
-    
 }
 
+ function createUser($http: ng.IHttpService) {
+        return new  UserService($http);
+    }
+
+
 export default angular.module('userService', [])
-    .factory('User', ['$http',userService]);
+    .factory('User', ['$http',createUser]);
