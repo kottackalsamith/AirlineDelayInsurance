5843e1c0dd49217d146582241b4ab40b64722e73
samith <kottackalsamith@gmail.com>
2016-11-22 11:26:27 +0530

    User controller converted to typescript and working

--- public/src/controllers/userCtrl.ts
+++ public/src/controllers/userCtrl.ts
@@ -5,17 +5,42 @@
 
 import '../services/userService';
 
+function userCreateController(User, $location, $window) {
+    var vm = this;
+    vm.signupUser = function () {
+        vm.message = '';
+        User.create(vm.userData)
+            .then(function (response) {
+                vm.userData = {};
+                vm.message = response.data.message;
+                $window.localStorage.setItem('token', response.data.token);
+                $location.path('/');
+            });
+    };
+}
+
+
+class UserCreateController {
+    public message: string;
+    public userData:Object;
+
+    static $inject = ['User', '$location', '$window'];
+
+    constructor(private User, private $location: ng.ILocationService, private $window: ng.IWindowService) {
+
+    }
+    signupUser() {
+        this.message = '';
+        console.log(this.userData);
+        this.User.create(this.userData)
+            .then(function (response) {
+                this.userData = {};
+                this.message = response.data.message;
+                this.$window.localStorage.setItem('token', response.data.token);
+                this.$location.path('/');
+            });
+    }
+}
+
 export default angular.module('userCtrl', ['userService'])
-    .controller('UserCreateController', function(User, $location, $window){
-        var vm = this;
-        vm.singupUser = function(){
-            vm.message = '';
-            User.create(vm.userData)
-                .then(function(response){
-                    vm.userData = {};
-                    vm.message = response.data.message;
-                    $window.localStorage.setItem('token', response.data.token);
-                    $location.path('/');
-                });
-        };
-    });
+    .controller('UserCreateController', userCreateController);
