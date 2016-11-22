d8e016bbb327591e787c1272bfcf300e756944c1
samith <kottackalsamith@gmail.com>
2016-11-22 19:27:09 +0530

    Insurance changing to typescript

--- public/app/controllers/insuranceCtrl.js
+++ public/app/controllers/insuranceCtrl.js
@@ -2,16 +2,18 @@
 System.register([], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
+    var InsuranceController;
     function insuranceController($rootScope, $scope, $http) {
         console.log('insurance controller');
+        var that = this;
         $http.get('api/insured')
             .then(function (response) {
-            $scope.content = response.data;
-            $scope.statuscode = response.status;
-            $scope.statustext = response.statustext;
-            $scope.content.forEach(function (data) {
+            that.content = response.data;
+            that.statuscode = response.status;
+            that.statustext = response.statustext;
+            that.content.forEach(function (data) {
                 if ($rootScope.userDetails.username === data.name) {
-                    $scope.name = data.name;
+                    that.name = data.name;
                     return true;
                 }
                 else {
@@ -23,8 +25,35 @@ System.register([], function(exports_1, context_1) {
     return {
         setters:[],
         execute: function() {
+            InsuranceController = (function () {
+                function InsuranceController($rootScope, $scope, $http) {
+                    this.$rootScope = $rootScope;
+                    this.$scope = $scope;
+                    this.$http = $http;
+                }
+                InsuranceController.prototype.getInsurance = function () {
+                    var _this = this;
+                    _this.$http.get('api/insured')
+                        .then(function (response) {
+                        _this.content = response.data;
+                        _this.statuscode = response.status;
+                        _this.statustext = response.statustext;
+                        _this.content.forEach(function (data) {
+                            if (_this.$rootScope.userDetails.username === data.name) {
+                                _this.name = data.name;
+                                return true;
+                            }
+                            else {
+                                return false;
+                            }
+                        });
+                    });
+                };
+                InsuranceController.$inject = ['$rootScope', '$scope', '$http'];
+                return InsuranceController;
+            }());
             exports_1("default",angular.module('insuranceCtrl', [])
-                .controller('InsuranceController', insuranceController));
+                .controller('InsuranceController', InsuranceController));
         }
     }
 });
--- public/src/controllers/insuranceCtrl.ts
+++ public/src/controllers/insuranceCtrl.ts
@@ -1,24 +1,60 @@
 // Controller for insurance tab
 
-function insuranceController ($rootScope, $scope, $http) {
-        console.log('insurance controller');
-        $http.get('api/insured')
+function insuranceController($rootScope, $scope, $http) {
+    console.log('insurance controller');
+    var that = this;
+    $http.get('api/insured')
+        .then(function (response) {
+            that.content = response.data;
+            that.statuscode = response.status;
+            that.statustext = response.statustext;
+            that.content.forEach(function (data) {
+                if ($rootScope.userDetails.username === data.name) {
+                    that.name = data.name;
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            });
+
+        });
+}
+
+class InsuranceController {
+    public content;
+    public response;
+    public statustext;
+    public statuscode;
+    public name;
+
+    static $inject = ['$rootScope', '$scope', '$http'];
+
+    constructor(private $rootScope: ng.IRootScopeService, private $scope: ng.IScope, private $http: ng.IHttpService) {
+
+    }
+
+    public getInsurance() {
+        var _this = this;
+        _this.$http.get('api/insured')
             .then(function (response) {
-                $scope.content = response.data;
-                $scope.statuscode = response.status;
-                $scope.statustext = response.statustext;
-                $scope.content.forEach(function (data) {
-                    if ($rootScope.userDetails.username === data.name){
-                        $scope.name= data.name;
+                _this.content = response.data;
+                _this.statuscode = response.status;
+                _this.statustext = response.statustext;
+                _this.content.forEach(function (data) {
+                    if (_this.$rootScope.userDetails.username === data.name) {
+                        _this.name = data.name;
                         return true;
                     }
-                    else{
+                    else {
                         return false;
                     }
                 });
 
-            });        
+            });
     }
+    
+}
 
 export default angular.module('insuranceCtrl', [])
-    .controller('InsuranceController', insuranceController);
+    .controller('InsuranceController', InsuranceController);
