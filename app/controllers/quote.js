2bed57ae5494977f30c51a900a553525fbecb8c5
samith <kottackalsamith@gmail.com>
2016-11-30 11:43:29 +0530

    Quote controller completely modularized

--- app/controllers/quote.js
+++ app/controllers/quote.js
@@ -8,6 +8,8 @@ var timeConversion = require('./quote_controllers/validations/timeconversion');
 
 
 var userinputValidation= require('./quote_controllers/validations/userinputvalidation');
+
+var quoteGeneration = require('./quote_controllers/quote-generation');
 exports.quoteCreate = function(req, res) {
 
     //Drop Collections
@@ -62,74 +64,9 @@ exports.quoteCreate = function(req, res) {
         accountCollection.accountCollectionCreation('London', 'Genneva', 5000);
         accountCollection.accountCollectionCreation('Zurich', 'Frankfurt', 6000);
         //Define function for implementing business logic
-        var quoteGeneration = function() {
-
-            //Find weather condition from 'Weather' collection with respect to  Date & Time
-            console.log('date and time for retrieving condition ' + datetime);
-            models.Weather.findOne({
-                "list.dt_txt": datetime
-            }, function(err, data) {
-                if (err)
-                    return console.error(err);
-                var weathercondition = data.list['description']; //Assign condition in 'weathercondition'
-                //Find Amount from 'Account' Collection with respect to Source & Destination
-                models.Account.findOne({
-                    "source": source,
-                    "destination": destination
-                }, function(err, data) {
-                    if (err) return console.error(err);
-                    var amount = data.amount; //Assign amount in 'amount'
-                    var noofusers = persons; //Assign persons in 'noofusers'
-                    console.log('xxxxxxVerifyxxxAmountxxxxxxx');
-                    console.log('Amount: ' + amount);
-                    console.log('Condition: ' + weathercondition);
-                    console.log('No of Users: ' + noofusers);
-                    console.log('xxxxxVerifyxxxxAmountxxxxxxx');
-                    var quote = 0; // Assign for quote amount
-                    //Checks Weather Conditions
-                    if (weathercondition == 'moderate rain') {
-                        quote = amount + (10 / 100);
-                    } else if (weathercondition == 'light rain') {
-                        quote = amount + (8 / 100);
-                    } else if (weathercondition == 'overcast clouds') {
-                        quote = amount + (6 / 100);
-                    } else if (weathercondition == 'broken clouds') {
-                        quote = amount + (4 / 100);
-                    } else if (weathercondition == 'scattered clouds') {
-                        quote = amount + (2 / 100);
-                    } else {
-                        quote = amount;
-                    }
-                    //Checks noofusers
-                    if (noofusers >= 3) {
-                        quote = quote - (15 / 100);
-                    } else if (noofusers == 2) {
-                        quote = quote - (10 / 100);
-                    } else {
-                        quote = quote;
-                    }
-                    //Sends Amount
-                    console.log('Exact Amount to be paid ' + quote);
-                    res.json({
-                        success: true,
-                        message: 'Total Quote',
-                        total: quote
-                    });
-
-                });
-
-            });
-
-
-        };
-        setTimeout(quoteGeneration, 2000);
+        quoteGeneration.quoteGeneration(datetime,source,destination,persons,res);
+        
 
     }
  
-    
-    
-   
-    
-
-
 };
